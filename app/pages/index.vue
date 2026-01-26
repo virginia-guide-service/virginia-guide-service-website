a916a5967e4260e5cadce6078d6613fd7a3d65c7
aleya <rjd8wv@virginia.edu>
2026-01-26 17:16:43 -0500

    client side

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -744,7 +744,7 @@ FRONT PAGE (virginiaguides.org)
         - Pagination dots for navigation
         - Links to Instagram posts for each event
         - Images with text descriptions below
-     ======================================== -->
+        ======================================== -->
     <section class="relative w-full">
         <!-- Semi-transparent Background Image Layer -->
         <div class="absolute inset-0">
@@ -774,76 +774,86 @@ FRONT PAGE (virginiaguides.org)
                 <!-- ========================================
                     RIGHT COLUMN: Event Image Carousel
                     Swiper slider with event cards linking to Instagram posts
+                    Wrapped in ClientOnly to prevent hydration mismatches
                     ======================================== -->
-                <swiper
-                    :slides-per-view="1"
-                    :slides-per-group="1"
-                    :space-between="30"
-                    :pagination="{ clickable: true }"
-                    :grabCursor="true"
-                    :autoplay="{ delay: 10000, disableOnInteraction: false, pauseOnMouseEnter: true }"
-                    :loop="true"
-                    :speed="800"
-                    :modules="modules"
-                    class="scrollElement events-swiper mx-auto max-w-4xl"
-                >
-                    <!-- Event Card 1: INCLEMENT WEATHER -->
-                    <swiper-slide class="flex justify-center items-start">
-                        <a href="https://www.instagram.com/p/DT6DdoiidTO/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
-                            <div class="relative max-w-2xl mx-auto p-4 pb-10 group cursor-pointer">
-                                <img class="events-image mb-2 object-top" loading="lazy" src="https://virginia-guides-website-images.s3.us-east-2.amazonaws.com/public/weather-recruitment-update.jpg">
-                                <p class="font-['Montserrat'] font-bold text-black text-2xl sm:text-3xl leading-tight mb-2">
-                                    Inclement Weather Recruitment Updates
-                                </p>
-                                <p class="font-['Montserrat'] font-medium text-base sm:text-lg leading-relaxed text-gray-900">
-                                    Despite the inclement weather, we're excited about what's ahead. Recruiting new guides, sharing history, and more. Check out
-                                    our Instagram to see new dates for our recruitment events this season and stay warm!
-                                </p>
-                            </div>
-                        </a>
-                    </swiper-slide>
+                <ClientOnly>
+                    <swiper
+                        :slides-per-view="1"
+                        :slides-per-group="1"
+                        :space-between="30"
+                        :pagination="{ clickable: true }"
+                        :grabCursor="true"
+                        :autoplay="{ delay: 10000, disableOnInteraction: false, pauseOnMouseEnter: true }"
+                        :loop="true"
+                        :speed="800"
+                        :modules="modules"
+                        class="scrollElement events-swiper mx-auto max-w-4xl"
+                    >
+                        <!-- Event Card 1: INCLEMENT WEATHER -->
+                        <swiper-slide class="flex justify-center items-start">
+                            <a href="https://www.instagram.com/p/DT6DdoiidTO/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                                <div class="relative max-w-2xl mx-auto p-4 pb-10 group cursor-pointer">
+                                    <img class="events-image mb-2 object-top" loading="lazy" src="https://virginia-guides-website-images.s3.us-east-2.amazonaws.com/public/weather-recruitment-update.jpg" alt="Weather Recruitment Update">
+                                    <p class="font-['Montserrat'] font-bold text-black text-2xl sm:text-3xl leading-tight mb-2">
+                                        Inclement Weather Recruitment Updates
+                                    </p>
+                                    <p class="font-['Montserrat'] font-medium text-base sm:text-lg leading-relaxed text-gray-900">
+                                        Despite the inclement weather, we're excited about what's ahead. Recruiting new guides, sharing history, and more. Check out
+                                        our Instagram to see new dates for our recruitment events this season and stay warm!
+                                    </p>
+                                </div>
+                            </a>
+                        </swiper-slide>
 
-                    <!-- Event Card 2: Shine with Guides -->
-                    <swiper-slide class="flex justify-center items-start">
-                        <a href="https://www.instagram.com/p/DTqzVa_CfFw/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
-                            <div class="relative max-w-2xl mx-auto p-4 pb-10 group cursor-pointer">
-                                <!-- Event Image - 450px height, object-center crop -->
-                                <img class="events-image mb-2 object-top" loading="lazy" src="https://virginia-guides-website-images.s3.us-east-2.amazonaws.com/public/shine-with-guides.jpg">
-                                <!-- Event Title -->
-                                <p class="font-['Montserrat'] font-bold text-black text-2xl sm:text-3xl leading-tight mb-2">
-                                    Shine with Guides: Spring 2026 Recruitment
-                                </p>
-                                <!-- Event Description -->
-                                <p class="font-['Montserrat'] font-medium text-base sm:text-lg leading-relaxed text-gray-900">
-                                    Join us on the of last week of January to hear from current guides about what we do and how you can become a guide!
-                                    Learn more about the recruitment process, ask questions, meet current tour guides, and sign up for your trial tour.
-                                    Info sessions are mandatory for potential guides to sign up for the next step in recruitment. Learn more about the process
-                                    on our <a class="underline text-blue-500" href="/join-guides">"Join Guides"</a> page.
-                                </p>
-                            </div>
-                        </a>
-                    </swiper-slide>
-
-                    <!-- Event Card 3: Meet the -->
-                    <swiper-slide class="flex justify-center items-start">
-                        <a href="https://www.instagram.com/p/DTyJN4yCfyo/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
-                            <div class="relative max-w-2xl mx-auto p-4 pb-10 group cursor-pointer">
-                                <img class="events-image mb-2 object-top" loading="lazy" src="https://virginia-guides-website-images.s3.us-east-2.amazonaws.com/public/meet-guides.jpg">
-                                <p class="font-['Montserrat'] font-bold text-black text-2xl sm:text-3xl leading-tight mb-2">
-                                    Meet Guides 
-                                </p>
-                                <p class="font-['Montserrat'] font-medium text-base sm:text-lg leading-relaxed text-gray-900">
-                                    DUE TO THE WINTER STORM THIS EVENT HAS BEEN MADE <a class="underline text-blue-500" href="https://virginia.zoom.us/j/93711736752?pwd=245K9MhR4PbbUrbdUSf1ptMVCKbaUm.1">VIRTUAL</a> (Passcode: 930753): 
-                                    Come hang out with current Guides on January 25 
-                                    from 4–6 PM! This is a great chance to learn more about the 
-                                    Virginia Guide Service, hear from current members, and ask any questions you have about finding your place 
-                                    in Guides. We’ll be hosting multiple sessions created with and for APID, Black, Latinx, and Queer students, 
-                                    so you can join the space that feels right for you.
-                                </p>
-                            </div>
-                        </a>
-                    </swiper-slide>
-                </swiper>
+                        <!-- Event Card 2: Shine with Guides -->
+                        <swiper-slide class="flex justify-center items-start">
+                            <a href="https://www.instagram.com/p/DTqzVa_CfFw/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                                <div class="relative max-w-2xl mx-auto p-4 pb-10 group cursor-pointer">
+                                    <!-- Event Image - 450px height, object-center crop -->
+                                    <img class="events-image mb-2 object-top" loading="lazy" src="https://virginia-guides-website-images.s3.us-east-2.amazonaws.com/public/shine-with-guides.jpg" alt="Shine with Guides">
+                                    <!-- Event Title -->
+                                    <p class="font-['Montserrat'] font-bold text-black text-2xl sm:text-3xl leading-tight mb-2">
+                                        Shine with Guides: Spring 2026 Recruitment
+                                    </p>
+                                    <!-- Event Description -->
+                                    <p class="font-['Montserrat'] font-medium text-base sm:text-lg leading-relaxed text-gray-900">
+                                        Join us on the of last week of January to hear from current guides about what we do and how you can become a guide!
+                                        Learn more about the recruitment process, ask questions, meet current tour guides, and sign up for your trial tour.
+                                        Info sessions are mandatory for potential guides to sign up for the next step in recruitment. Learn more about the process
+                                        on our <a class="underline text-blue-500" href="/join-guides">"Join Guides"</a> page.
+                                    </p>
+                                </div>
+                            </a>
+                        </swiper-slide>
+                        
+                        <!-- Event Card 3: Meet the Guides -->
+                        <swiper-slide class="flex justify-center items-start">
+                            <a href="https://www.instagram.com/p/DTyJN4yCfyo/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                                <div class="relative max-w-2xl mx-auto p-4 pb-10 group cursor-pointer">
+                                    <img class="events-image mb-2 object-top" loading="lazy" src="https://virginia-guides-website-images.s3.us-east-2.amazonaws.com/public/meet-guides.jpg" alt="Meet Guides">
+                                    <p class="font-['Montserrat'] font-bold text-black text-2xl sm:text-3xl leading-tight mb-2">
+                                        Meet Guides 
+                                    </p>
+                                    <p class="font-['Montserrat'] font-medium text-base sm:text-lg leading-relaxed text-gray-900">
+                                        DUE TO THE WINTER STORM THIS EVENT HAS BEEN MADE <a class="underline text-blue-500" href="https://virginia.zoom.us/j/93711736752?pwd=245K9MhR4PbbUrbdUSf1ptMVCKbaUm.1">VIRTUAL</a> (Passcode: 930753): 
+                                        Come hang out with current Guides on January 25 
+                                        from 4–6 PM! This is a great chance to learn more about the 
+                                        Virginia Guide Service, hear from current members, and ask any questions you have about finding your place 
+                                        in Guides. We'll be hosting multiple sessions created with and for APID, Black, Latinx, and Queer students, 
+                                        so you can join the space that feels right for you.
+                                    </p>
+                                </div>
+                            </a>
+                        </swiper-slide>
+                    </swiper>
+                    
+                    <!-- Fallback content while loading (optional but recommended) -->
+                    <template #fallback>
+                        <div class="scrollElement events-swiper mx-auto max-w-4xl h-96 flex items-center justify-center">
+                            <p class="text-gray-500">Loading events...</p>
+                        </div>
+                    </template>
+                </ClientOnly>
 
             </div>
         </div>
